947af07a908a4d087f15228033867935b0d7cc13
Timothy C. Lam <geroginalam@hotmail.com>
2019-09-19 01:54:51 +0800

    update library

--- stdlib/library.go
+++ stdlib/library.go
@@ -65,66 +65,6 @@ func Traverse(instance interface{}, relationPath string, logicToExecute interfac
 	}
 }
 
-// Retrieve all the elements from the given relation path
-func traverseRetrievePath(instance interface{}, relationPath string) []interface{} {
-	var retrievedInstances []interface{}
-	paths := strings.Split(relationPath, ".")
-
-	// Go through all the paths in parameter two
-	for _, path := range paths {
-		instanceValue := reflect.ValueOf(instance)
-		retrievedInstances = nil
-
-		// Check if current value in instance is slice
-		if instanceValue.Kind() == reflect.Slice {
-			// Call the method for each instance in instance
-			for i := 0; i < instanceValue.Len(); i++ {
-				retrievedInstances = traverseInsertToSlice(reflect.ValueOf(instanceValue.Index(i).Interface()).MethodByName(path).Call(nil)[0].Interface(), retrievedInstances)
-			}
-		} else {
-			// Set the instance to the current unary relation
-			retrievedInstances = traverseInsertToSlice(instanceValue.MethodByName(path).Call(nil)[0].Interface(), retrievedInstances)
-		}
-
-		// Update instance with the next set of retrievedInstances to be traversed
-		instance = retrievedInstances
-	}
-
-	return retrievedInstances
-}
-
-// If retrieve value is slice, traverse them 1 by 1 to store them
-func traverseInsertToSlice(retrievedInstance interface{}, storeSlice []interface{}) []interface{} {
-	retrievedInstanceValue := reflect.ValueOf(retrievedInstance)
-
-	if retrievedInstanceValue.Kind() == reflect.Slice {
-		for i := 0; i < retrievedInstanceValue.Len(); i++ {
-			storeSlice = append(storeSlice, retrievedInstanceValue.Index(i).Interface())
-		}
-	} else {
-		storeSlice = append(storeSlice, retrievedInstanceValue.Interface())
-	}
-
-	return storeSlice
-}
-
-// Check for correctness of input of traverse parameter
-func traverseCheckError(instance interface{}, logicToExecute interface{}) (isValid bool, err string) {
-	instanceValue := reflect.ValueOf(instance)
-
-	if instanceValue.Kind() == reflect.Slice {
-		// Panic if first parameter is not unary
-		err = "non unary relation in first parameter"
-	} else if instanceValue.Kind() != reflect.Ptr {
-		err = "non relation passed to first parameter for Traverse"
-	} else if reflect.ValueOf(logicToExecute).Kind() != reflect.Func {
-		err = "non function passed to the third parameter"
-	}
-
-	isValid = err == ""
-	return
-}
-
 // Counter counts all the elements with the filter applied
 func Counter(instance interface{}, relationPath string, logicToExecute interface{}) (count int) {
 	if isValid, err := traverseCheckError(instance, logicToExecute); !isValid {
