c47a368aa06786d88c3b0feedc9fb4f1475ecc95
Timothy C. Lam <geroginalam@hotmail.com>
2020-01-11 16:47:49 +0800

    remove QT

--- ui/mainwindow.go
+++ ui/mainwindow.go
@@ -1,38 +1,32 @@
 package ui
 
-import (
-	"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/widgets"
-	"github.com/therecipe/qt/uitools"
-)
-
 // MainWindow struct
-type MainWindow struct {
-	Close func() bool
-	PushButtonExit *widgets.QPushButton
-	widget *widgets.QWidget
-}
-
-func (c *MainWindow) init(formWidget *widgets.QWidget) {
-	c.widget = formWidget
-	c.Close = formWidget.Close
-	c.PushButtonExit = widgets.NewQPushButtonFromPointer(formWidget.FindChild("pushButtonExit", core.Qt__FindChildrenRecursively).Pointer())
-
-	c.PushButtonExit.ConnectPressed( func() {
-		c.Close()
-	})
-}
-
-// NewMainWindow xaxa
-func NewMainWindow() *widgets.QWidget {
-	file := core.NewQFile2("./ui/mainwindow.ui")
-	file.Open(core.QIODevice__ReadOnly)
-	formWidget := uitools.NewQUiLoader(nil).Load(file, nil)
-	file.Close()
-
-	mw := MainWindow{}
-
-	mw.init(formWidget)
-
-	return formWidget
-}
+// type MainWindow struct {
+// 	Close func() bool
+// 	PushButtonExit *widgets.QPushButton
+// 	widget *widgets.QWidget
+// }
+//
+// func (c *MainWindow) init(formWidget *widgets.QWidget) {
+// 	c.widget = formWidget
+// 	c.Close = formWidget.Close
+// 	c.PushButtonExit = widgets.NewQPushButtonFromPointer(formWidget.FindChild("pushButtonExit", core.Qt__FindChildrenRecursively).Pointer())
+//
+// 	c.PushButtonExit.ConnectPressed( func() {
+// 		c.Close()
+// 	})
+// }
+//
+// // NewMainWindow xaxa
+// func NewMainWindow() *widgets.QWidget {
+// 	file := core.NewQFile2("./ui/mainwindow.ui")
+// 	file.Open(core.QIODevice__ReadOnly)
+// 	formWidget := uitools.NewQUiLoader(nil).Load(file, nil)
+// 	file.Close()
+//
+// 	mw := MainWindow{}
+//
+// 	mw.init(formWidget)
+//
+// 	return formWidget
+// }
